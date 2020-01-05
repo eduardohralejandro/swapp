aaab65c2472c91b71d2c0799cb486cdeb8c17a9a
Eduardo Hernandez <eduardo@chatbot-plus.com>
2020-01-05 20:43:11 +0100

    new message heroku

--- client/server.js
+++ client/server.js
@@ -4,11 +4,10 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './src/index.html'));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port);

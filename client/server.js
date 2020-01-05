5c96cf7ea092020904d648ba497e1fbafaa7bf39
Eduardo Hernandez <eduardo@chatbot-plus.com>
2020-01-05 20:46:30 +0100

    new configuration, express app plus heroku

--- client/server.js
+++ client/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function(req, res) {
